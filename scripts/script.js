b93f08f8cabfd7d35e80591e3a3244d28b93306c
Bruno Hemsi <brunofish.slim@gmail.com>
2025-05-16 20:48:44 -0300

    botão anonimo funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -11,18 +11,14 @@ function sendMail(){
 
     const email_checker = parms.form_email.split("@");
 
-    if (document.getElementById("anon").checked && parms.form_email != "" && parms.form_message != ""){
-        parms.form_name = "anonimo"
-        emailjs.send("service_MassageEmail","template_contact",parms).then(alert("mensagem enviada com sucesso!"))
-        location.reload();
-    } else if (parms.form_name == "" || parms.form_email == "" || parms.form_message == ""){
+    if (parms.form_name == "" || parms.form_email == "" || parms.form_message == ""){
         alert("preencha todos os campos")
         return;
     } else if (email_checker.length != 2){
         alert("email invalido")
         return;
     } else {
-        emailjs.send("service_MassageEmail","template_contact",parms).then(alert("mensagem enviada com sucesso!"))
-        location.reload();
+        emailjs.send("service_MassageEmail","template_contact",parms).then(alert("mensagem enviada com sucesso!"));
+        // location.reload();
     }
 }
